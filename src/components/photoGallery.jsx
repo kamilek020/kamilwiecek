97025ef6325f854bc7477722780a081e70050017
kamilek020 <kamil.wiecek.02@icloud.com>
2023-06-29 21:17:42 +0200

    fix photos - part 2

--- src/components/photoGallery.jsx
+++ src/components/photoGallery.jsx
@@ -3,14 +3,14 @@ import React, { useState, useEffect, useRef,  } from 'react';
 
 const PhotoGallery = () => {
     const photos = [
-        { id: 1, url: './images/astronauta-projektor.jpg', description: 'PROJEKTOR ASTRONAUTA', details: 'Szczegóły produktu 1' },
-        { id: 2, url: './images/led510.jpg', description: 'TAŚMA LED 5 METRÓW', details: 'Szczegóły produktu 1'  },
-        { id: 3, url: './images/led510.jpg', description: 'TAŚMA LED 10 METRÓW', details: 'Szczegóły produktu 1'  },
-        { id: 4, url: './images/sunset-lamp.jpg', description: 'SUNSET LAMPA', details: 'Szczegóły produktu 1'  },
-        { id: 5, url: './images/orgaznier-bizu.png', description: 'ORGANIZER NA BIŻUTERIĘ', details: 'Szczegóły produktu 1'  },
-        { id: 6, url: './images/projektor-zwykly.jpg', description: 'PROJEKTOR GWIAZD', details: 'Szczegóły produktu 1'  },
-        { id: 7, url: './images/projektor-zwykly.jpg', description: 'PROJEKTOR GWIAZD', details: 'Szczegóły produktu 1'  },
-        { id: 8, url: './images/projektor-zwykly.jpg', description: 'PROJEKTOR GWIAZD', details: 'Szczegóły produktu 1'  },
+        { id: 1, url: '../../images/astronauta-projektor.jpg', description: 'PROJEKTOR ASTRONAUTA', details: 'Szczegóły produktu 1' },
+        { id: 2, url: '../../images/led510.jpg', description: 'TAŚMA LED 5 METRÓW', details: 'Szczegóły produktu 1'  },
+        { id: 3, url: '../../images/led510.jpg', description: 'TAŚMA LED 10 METRÓW', details: 'Szczegóły produktu 1'  },
+        { id: 4, url: '../../images/sunset-lamp.jpg', description: 'SUNSET LAMPA', details: 'Szczegóły produktu 1'  },
+        { id: 5, url: '../../images/orgaznier-bizu.png', description: 'ORGANIZER NA BIŻUTERIĘ', details: 'Szczegóły produktu 1'  },
+        { id: 6, url: '../../images/projektor-zwykly.jpg', description: 'PROJEKTOR GWIAZD', details: 'Szczegóły produktu 1'  },
+        { id: 7, url: '../../images/projektor-zwykly.jpg', description: 'PROJEKTOR GWIAZD', details: 'Szczegóły produktu 1'  },
+        { id: 8, url: '../../images/projektor-zwykly.jpg', description: 'PROJEKTOR GWIAZD', details: 'Szczegóły produktu 1'  },
     ];
     const [selectedProduct, setSelectedProduct] = useState(null);
 
